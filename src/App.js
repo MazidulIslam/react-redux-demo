eea0d4d63addeb9f2bcb012f0187d62e83a095ef
MazidulIslam <56502659+MazidulIslam@users.noreply.github.com>
2021-12-18 01:13:20 +0600

    add item container and use function and ternary operator in map functions

--- src/App.js
+++ src/App.js
@@ -7,11 +7,14 @@ import IceCreamContainer from "./components/IceCreamContainer";
 import ChocolateContainer from "./components/ChocolateContainer";
 import HooksChocolateContainer from "./components/HooksChocolateContainer";
 import NewCakeContainer from "./components/NewCakeContainer";
+import ItemContainer from "./components/ItemContainer";
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <ItemContainer cake />
+        <ItemContainer />
         <CakeContainer />
         <HooksCakeContainer />
         <IceCreamContainer />
