387dc472eebf7b6193f207a5c292800a4b4079af
MazidulIslam <56502659+MazidulIslam@users.noreply.github.com>
2021-12-17 12:10:55 +0600

    add mapState & mapDispatchToProps function and connect to react-redux

--- src/components/CakeContainer.js
+++ src/components/CakeContainer.js
@@ -1,12 +1,26 @@
 import React from "react";
+import { connect } from "react-redux";
+import { buyCake } from "../redux";
 
-const CakeContainer = () => {
+const CakeContainer = (props) => {
   return (
     <div>
-      <h1>Number of Cakes</h1>
-      <button>Buy Cake</button>
+      <h1>Number of Cakes - {props.numOfCakes}</h1>
+      <button onClick={props.buyCake}>Buy Cake</button>
     </div>
   );
 };
 
-export default CakeContainer;
+const mapStateToProps = (state) => {
+  return {
+    numOfCakes: state.numOfCakes,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    buyCake: () => dispatch(buyCake()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(CakeContainer);
