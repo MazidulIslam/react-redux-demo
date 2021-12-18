330dffcd27c98c8a70492bdaebb6f1a6881589a9
MazidulIslam <56502659+MazidulIslam@users.noreply.github.com>
2021-12-18 12:27:32 +0600

    fetch user and display

--- src/App.js
+++ src/App.js
@@ -8,11 +8,14 @@ import ChocolateContainer from "./components/ChocolateContainer";
 import HooksChocolateContainer from "./components/HooksChocolateContainer";
 import NewCakeContainer from "./components/NewCakeContainer";
 import ItemContainer from "./components/ItemContainer";
+import UserContainer from "./components/UserContainer";
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <UserContainer />
+        <h1>End of User List</h1>
         <ItemContainer cake />
         <ItemContainer />
         <CakeContainer />
--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import cakeReducer from "./cake/cakeReducer";
 import chocolateReducer from "./chocolate/chocolateReducer";
 import iceCreamReducer from "./iceCream/iceCreameReducer";
+import userReducer from "./user/userReducer";
 
 const rootReducer = combineReducers({
   cake: cakeReducer,
   iceCream: iceCreamReducer,
   chocolate: chocolateReducer,
+  user: userReducer,
 });
 
 export default rootReducer;
